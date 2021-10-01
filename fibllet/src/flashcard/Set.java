763d7e181683f60d885fa27661bc1d56c626f503
Adam <adamanikiej1@gmail.com>
2021-10-01 11:29:08 -0400

    Added ability to double click a set in the create/edit set scene.

--- fibllet/src/flashcard/Set.java
+++ fibllet/src/flashcard/Set.java
@@ -12,6 +12,14 @@ public class Set {
 	private LocalDate creationDate;
 	private ArrayList<String> relatedQuizzes;
 
+	/**
+	 * Constructor Used for displaying Sets in tableview
+	 * 
+	 * @param name
+	 * @param count
+	 * @param date
+	 * @param relatedQuizzes
+	 */
 	public Set(String name, int count, LocalDate date, ArrayList<String> relatedQuizzes) {
 		this.name = new SimpleStringProperty(name);
 		this.cardCount = new SimpleIntegerProperty(count);
@@ -21,27 +29,59 @@ public class Set {
 		this.relatedQuizzes = relatedQuizzes;
 	}
 
+	/**
+	 * Getter for tableview
+	 * 
+	 * @return String of set name
+	 */
 	public String getSetName() {
 		return name.get();
 	}
 
+	/**
+	 * Getter for tableview
+	 * 
+	 * @return Integer of card count in set
+	 */
 	public Integer getCardCount() {
 		return cardCount.get();
 	}
 
+	/**
+	 * Getter for tableview
+	 * 
+	 * @return String of creation date of set "Month Day, Year" format
+	 */
 	public String getCreationDate() {
 		return creationDate.getMonth().toString().charAt(0)
 				+ creationDate.getMonth().toString().substring(1).toLowerCase() + " " + creationDate.getDayOfMonth()
 				+ ", " + creationDate.getYear();
 	}
 
+	/**
+	 * Getter for tableview
+	 * 
+	 * @return String of related quizzes for given set "relatedQuiz1, relatedQuiz2"
+	 *         format
+	 */
 	public String getRelatedQuizzes() {
 		String full = "";
 
-		for (String related : relatedQuizzes) {
-			full += " " + related;
+		for (int i = 0; i < relatedQuizzes.size() - 1; i++) {
+			full += relatedQuizzes.get(i) + ", ";
+		}
+		if (relatedQuizzes.size() > 0) {
+			full += relatedQuizzes.get(relatedQuizzes.size() - 1);
 		}
 		return full;
+	}
+
+	@Override
+	public String toString() {
+		String result = "";
+		result += "Set Name: " + getSetName() + "\nCard Count: " + getCardCount() + "\nCreation Date: "
+				+ getCreationDate() + "\nRelated Quizzes: " + getRelatedQuizzes();
+		return result;
 	}
 
 }
--- fibllet/src/newsetmenu/NewSetController.java
+++ fibllet/src/newsetmenu/NewSetController.java
@@ -25,42 +25,7 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 public class NewSetController implements Initializable {
-	
-	//setting up set table
-	@FXML private TableView<Set> tableView;
-	@FXML private TableColumn<Set, String> setNameColumn;
-	@FXML private TableColumn<Set, Integer> cardCountColumn;
-	@FXML private TableColumn<Set, String> creationDateColumn;
-	@FXML private TableColumn<Set, String> relatedQuizzesColumn;
-	
-	
-	
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		//set up columns for new Set
-		setNameColumn.setCellValueFactory(new PropertyValueFactory<Set, String>("setName"));
-		cardCountColumn.setCellValueFactory(new PropertyValueFactory<Set, Integer>("cardCount"));
-		creationDateColumn.setCellValueFactory(new PropertyValueFactory<Set, String>("creationDate"));
-		relatedQuizzesColumn.setCellValueFactory(new PropertyValueFactory<Set, String>("relatedQuizzes"));
-		
-		//populate set with data for testing
-		tableView.setItems(getSets());
-		
-	}
-	
-	public ObservableList<Set> getSets() {
-		ObservableList<Set> sets = FXCollections.observableArrayList();
-		
-		ArrayList<String> related1 = new ArrayList<String>();
-		related1.add("Anatomy");
-		related1.add("Quiz 1");
-		
-		sets.add(new Set("Arm Anatomy", 13, LocalDate.of(2021, Month.SEPTEMBER, 21), related1));
-		sets.add(new Set("Leg Anatomy", 21, LocalDate.of(2020, Month.DECEMBER, 4), related1));
-		
-		return sets;
-	}
-	
+
 	@FXML
 	AnchorPane application;
 
@@ -69,6 +34,71 @@ public class NewSetController implements Initializable {
 
 	@FXML
 	Button menuBtn;
+	
+	// setting up set table
+	@FXML
+	private TableView<Set> tableView;
+	@FXML
+	private TableColumn<Set, String> setNameColumn;
+	@FXML
+	private TableColumn<Set, Integer> cardCountColumn;
+	@FXML
+	private TableColumn<Set, String> creationDateColumn;
+	@FXML
+	private TableColumn<Set, String> relatedQuizzesColumn;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// set up columns for new Set
+		setNameColumn.setCellValueFactory(new PropertyValueFactory<Set, String>("setName"));
+		cardCountColumn.setCellValueFactory(new PropertyValueFactory<Set, Integer>("cardCount"));
+		creationDateColumn.setCellValueFactory(new PropertyValueFactory<Set, String>("creationDate"));
+		relatedQuizzesColumn.setCellValueFactory(new PropertyValueFactory<Set, String>("relatedQuizzes"));
+
+		// populate set with data for testing
+		tableView.setItems(getSets());
+
+	}
+
+	/**
+	 * Function used to construct list of all items that should be put into
+	 * tableview
+	 * 
+	 * @return
+	 */
+	public ObservableList<Set> getSets() {
+		ObservableList<Set> sets = FXCollections.observableArrayList();
+
+		ArrayList<String> related1 = new ArrayList<String>();
+		related1.add("Anatomy");
+		related1.add("Quiz 1");
+
+		sets.add(new Set("Arm Anatomy", 13, LocalDate.of(2021, Month.SEPTEMBER, 21), related1));
+		sets.add(new Set("Leg Anatomy", 21, LocalDate.of(2020, Month.DECEMBER, 4), related1));
+
+		return sets;
+	}
+	
+	/**
+	 * Function to manage if a set in tableview is double clicked
+	 * used to displayed the edit set scene
+	 * @param event
+	 */
+	@FXML
+	private void tableClick(MouseEvent event) {
+		if (event.getClickCount() == 2) {
+			System.out.println("double clicked set");
+			System.out.println(tableView.getSelectionModel().getSelectedItem());
+		}
+	}
+	
+	/*
+	tableView.setOnMouseClicked((MouseEvent event) -> {
+        if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2){
+            System.out.println(table.getSelectionModel().getSelectedItem());
+        }
+    });
+    */
 
 	@FXML
 	private void exitBtnClick() {
@@ -130,6 +160,5 @@ public class NewSetController implements Initializable {
 	private void toolBarReleased(MouseEvent evt) {
 		resetMoveOperation();
 	}
-	
-	
+
 }
