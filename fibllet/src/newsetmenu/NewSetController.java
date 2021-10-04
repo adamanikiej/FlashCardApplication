2e02f7309f5f471a71f158a6702b62918055bb97
Adam <adamanikiej1@gmail.com>
2021-10-03 21:07:42 -0400

    edit/create set menu highlighting and dehighlighting of tableview

--- fibllet/src/newsetmenu/NewSetController.java
+++ fibllet/src/newsetmenu/NewSetController.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 import flashcard.Set;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -16,8 +17,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -25,6 +28,9 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 public class NewSetController implements Initializable {
+	//TODO
+	//make it so when you double click, it goes to edit set menu
+	//make it when you right click it brings up mini menu of edit set, rename set, delete set, create quiz set
 
 	@FXML
 	AnchorPane application;
@@ -34,7 +40,7 @@ public class NewSetController implements Initializable {
 
 	@FXML
 	Button menuBtn;
-	
+
 	// setting up set table
 	@FXML
 	private TableView<Set> tableView;
@@ -58,6 +64,60 @@ public class NewSetController implements Initializable {
 		// populate set with data for testing
 		tableView.setItems(getSets());
 
+		// set up event handler for tableview rows
+		EventHandler<MouseEvent> onClick = this::handleRowMouseClick;
+		tableView.setRowFactory(param -> {
+			TableRow<Set> row = new TableRow<>();
+			row.setOnMouseClicked(onClick);
+			return row;
+
+		});
+
+	}
+
+	private void handleRowMouseClick(MouseEvent event) {
+		@SuppressWarnings("unchecked")
+		TableRow<Set> row = (TableRow<Set>) event.getSource();
+		//System.out.println("mouse clicked:" + row.isEmpty());
+
+		if (event.getButton() == MouseButton.PRIMARY) {
+			if (row.isEmpty()) {
+				// remove selection
+				row.getTableView().getSelectionModel().clearSelection();
+			} else if (event.getClickCount() == 2 && row.getItem() != null) {
+				System.out.println("double clicked set");
+				System.out.println(row.getItem());
+				event.consume();
+			}
+		}
+		// use row.isEmpty() to determine if clicking on row or not
+		/*
+		 * Logic: if event is primary and is double click -> if row is not empty and row
+		 * item is not empty go to set editor deselect row else deselect row? else if
+		 * event is primary and is single click and row is empty and row item is empty
+		 * if
+		 */
+
+		/*
+		 * if (event.getButton() == MouseButton.PRIMARY && (event.getClickCount() == 1)
+		 * || event.getClickCount() == 2) { if (row != null) {
+		 * row.getTableView().getSelectionModel().clearSelection(); } }
+		 */
+
+		// if double clicked on a set
+		/*
+		if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
+			if (!row.isEmpty() && row.getItem() != null) {
+				// editSet() -> loads set in edit scene mode
+				System.out.println("double clicked set");
+				System.out.println(row.getItem());
+				event.consume();
+			} else if (event.getClickCount() == 1) {
+				row.getTableView().getSelectionModel().clearSelection();
+			}
+		}
+		*/
+		// row.getTableView().getSelectionModel().clearSelection();
 	}
 
 	/**
@@ -78,27 +138,7 @@ public class NewSetController implements Initializable {
 
 		return sets;
 	}
-	
-	/**
-	 * Function to manage if a set in tableview is double clicked
-	 * used to displayed the edit set scene
-	 * @param event
-	 */
-	@FXML
-	private void tableClick(MouseEvent event) {
-		if (event.getClickCount() == 2) {
-			System.out.println("double clicked set");
-			System.out.println(tableView.getSelectionModel().getSelectedItem());
-		}
-	}
-	
-	/*
-	tableView.setOnMouseClicked((MouseEvent event) -> {
-        if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2){
-            System.out.println(table.getSelectionModel().getSelectedItem());
-        }
-    });
-    */
+
 
 	@FXML
 	private void exitBtnClick() {
